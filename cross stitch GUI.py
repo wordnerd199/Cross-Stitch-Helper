e793cd061534665b61827ef6ee083cf048459b09
wordnerd199 <90263910+wordnerd199@users.noreply.github.com>
2021-12-01 18:38:51 -0500

    Add files via upload
    Changed the thread length calculation logic--now it calculates the length of the stitches, then multiplies that by two (since the thread will be held double when actually working) and adds an extra 8 inches to the total to account for tying off/floating between sections. The total is still output as inches, and it is rounded up to an integer using math.ceil(). This change has only been made to the GUI version of the application, as that is what will be submitted in the final project.

---
+++ cross stitch GUI.py
@@ -0,0 +1,69 @@
+import tkinter as tk
+import math
+
+def file_check(f):
+    try:
+        r = open(f, "r")
+        r.close()
+        return True
+    except:
+        return False
+
+def stitch_measure(count):
+    stitch = 1/count
+    diagonal = math.sqrt(2*(stitch**2))
+    thread_length = (2*stitch) + (2*diagonal)
+    return thread_length
+
+def Convert_file():
+    file_name = fileName.get()
+    if file_check(file_name) == False:
+        lblOutput["text"] = "File not found."
+    else:
+        file = open(file_name, "r")
+        file_contents = file.read()
+        file.close()
+        split_file = file_contents.split("\n")
+        pattern = []
+        for i in range(len(split_file)):
+            row = split_file[i].split(",")
+            pattern.append(row)
+        colors = {}
+        for i in range(len(pattern)):
+            for x in range(len(pattern[i])):
+                color = pattern[i][x]
+                if color == "":
+                    pattern[i].remove(color)
+                elif color not in colors.keys():
+                    colors[color] = 1
+                else:
+                    colors[color] = colors.get(color) + 1
+        names = list(colors.keys())
+        output = []
+        count = int(fabricCount.get())
+        thread_per_stitch = stitch_measure(count)
+        for i in range(len(names)):
+            symbol = names[i]
+            length = math.ceil((thread_per_stitch*colors[symbol]*2) + 8)
+            output.append(str(length) + " inches of thread in color " + symbol)
+        lblOutput["text"] = "\n".join(output)
+    
+window = tk.Tk()
+window.title("Cross stitch helper")
+fileName = tk.Entry(width = 15)
+fabricCount = tk.Entry(width = 5)
+lbl_fileName = tk.Label(text = "Pattern file name: ")
+lbl_fabricCount = tk.Label(text = "Fabric count: ")
+btnConvert = tk.Button(
+    master = window,
+    text = "Analyze pattern",
+    width = 17,
+    height = 1,
+    command = Convert_file)
+lblOutput = tk.Label(text = "")
+fileName.grid(row = 0, column = 1)
+lbl_fileName.grid(row = 0, column = 0)
+fabricCount.grid(row = 1, column = 1)
+lbl_fabricCount.grid(row = 1, column = 0)
+btnConvert.grid(row = 2, column = 1)
+lblOutput.grid(row = 3)
